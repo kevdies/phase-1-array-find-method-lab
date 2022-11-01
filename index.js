af5cfe45ed28ab3fb4c4a015c5b1222db385d5a7
kevin diesenberg <kdiesenb@gmail.com>
2022-11-01 17:40:07 -0400

    commit tree f5e652dc9614fc51623b8cad1b031e653f31a213

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+
+// const superbowlWin = objArray => {
+//     for (const element of objArray) {
+//         if (element.result === "W") {
+//             return element.year;
+//         }
+//     }
+// }
+
+const superbowlWin = function(objArray) {
+   const found = objArray.find((record) => {
+    return record.result === "W"
+
+   })
+    // if (found) {
+    //     return found.year;
+    // } else {
+    //     return undefined;
+    // }
+    return found ? found.year : undefined;
+}
+// console.log(superbowlWin(records))
